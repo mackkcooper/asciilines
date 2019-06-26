4200a0c66e269b06e9fcfbc9b7832905ed0a8192
Mack Cooper <maqenzi@gmail.com>
2019-06-26 11:27:10 -0700

    fill graphical array with parsed lines

--- asciilines.py
+++ asciilines.py
@@ -1,6 +1,6 @@
 import sys
 
-background = ' '
+background = '.'
 
 # find tvg file
 if (len(sys.argv) != 2): 
@@ -13,12 +13,12 @@ file.close()
 
 # parse tvg file
 line = content[0].split(' ')
-if(len(line) != 2):
+if (len(line) != 2):
     print("ERROR: File provided was unable to be parsed due to formatting.")
     exit(1)
 height = int(line[0])
 width = int(line[1])
-if(height < 1 or width < 1):
+if (height < 1 or width < 1):
     print("ERROR: File provided has canvas height and/or width values less than 1.")
     exit(1)
 
@@ -26,6 +26,29 @@ if(height < 1 or width < 1):
 canvas = [[background] * width for i in range(height)]
 
 # fill graphical array
+for l in range(1,len(content)):
+    line = content[l].split(' ')
+    if (len(line) != 5):
+        print("ERROR: File provided was unable to be parsed due to formatting.")
+        exit(1)
+    char = line[0]
+    h = int(line[1])
+    w = int(line[2])
+    direction = line[3]
+    length = int(line[4])
+    if (direction == 'v'): #vertical
+        if (w > -1 and w < width):
+            for i in range(length):
+                if ((h + i) > -1 and (h + i) < height):
+                    canvas[h + i][w] = char
+    elif (direction == 'h'):  #horizontal
+        if (h > -1 and h < height):
+            for i in range(length):
+                if ((w + i) > -1 and (w + i) < width):
+                    canvas[h][w + i] = char
+    else:
+        print("ERROR: File provided was unable to be parsed due to formatting.")
+        exit(1)
 
 # ouput graphical array to user
 for row in canvas:
