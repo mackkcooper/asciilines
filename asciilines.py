23081b3cddefae91265eabb09a88156cd7e1abb3
Mack Cooper <maqenzi@gmail.com>
2019-06-25 18:02:24 -0700

    file import added

--- asciilines.py
+++ asciilines.py
@@ -1,4 +1,12 @@
+import sys
+
 # find tvg file
+if (len(sys.argv) != 2): 
+    print("ERROR: No argument provided. Please provide the path to the .tvg file you wish to have translated.")
+    exit()
+filename = sys.argv[1]
+file = open(filename, 'r')
+content = file.readlines()
 
 # parse tvg file
 
